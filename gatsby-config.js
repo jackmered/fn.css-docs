80c79ae9acd9d0ddfc8828feefc2f067d57aa0cb
Jack Meredith <jackmered@gmail.com>
2018-06-05 19:40:38 +0100

    Adde GA plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,14 @@ module.exports = {
   siteMetadata: {
     title: 'fn.css - Docs',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-120403674-1",
+        head: false,
+      },
+    }
+  ],
 };

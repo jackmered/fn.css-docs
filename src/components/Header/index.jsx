07054372c40be4f426d432c510cd41ffab347a17
Jack Meredith <jackmered@gmail.com>
2020-07-26 17:41:57 +0100

    Added logo and favicon

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,26 +1,33 @@
 import React from "react";
 import Link from "gatsby-link";
+import logo from "../../images/fn-css-logo-reverse.svg";
 
 const Header = () => (
   <header className="header bg-primary psf t0 l0 w100">
     <div className="container x h100 xac px1-5">
-      <h1 className="m0 h100">
+      <h1 className="m0">
         <Link to="/" className="tdn cl-white x h100 fwn">
-          fn.css
+          <img src={logo} alt="fn.css" width="40" height="40" />
         </Link>
       </h1>
       <nav className="mla h100 x">
-        <Link to="/docs" className="tdn cl-white px1 h100 x xac">
+        <Link
+          to="/docs"
+          className="tdn cl-white bg-primary px1 h100 x xac clh-primary bgh-white tdnh"
+        >
           Documentation
         </Link>
-        <Link to="/examples" className="tdn cl-white px1 h100 x xac">
+        <Link
+          to="/examples"
+          className="tdn cl-white bg-primary px1 h100 x xac clh-primary bgh-white tdnh"
+        >
           Examples
         </Link>
         <a
           href="https://www.npmjs.com/package/fn.css"
           target="_blank"
           rel="noopener"
-          className="tdn cl-white px1 h100 x xac"
+          className="tdn cl-white bg-primary px1 h100 x xac clh-primary bgh-white tdnh"
         >
           v3.1.0
         </a>
@@ -28,7 +35,7 @@ const Header = () => (
           href="https://github.com/jackmered/fn.css"
           target="_blank"
           rel="noopener"
-          className="tdn cl-white px1 h100 x xac"
+          className="tdn cl-white bg-primary px1 h100 x xac clh-primary bgh-white tdnh"
         >
           GitHub
         </a>
